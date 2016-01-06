d5e58cd221bd3b111a76d6e5169450aa8215d40f
Max Halford <maxhalford25@gmail.com>
2016-01-06 10:05:45 +0100

    Pour Marie

--- app/vues/conducteur.py
+++ app/vues/conducteur.py
@@ -1,10 +1,17 @@
 from flask import Blueprint, render_template
+from app.outils.geographie import geocoder
 
 # Créer un patron pour les vues conducteurs
 conducteurbp = Blueprint('conducteurbp', __name__, url_prefix='/conducteur')
 
 
-@conducteurbp.route('/accueil', methods=['GET', 'POST'])
+def gps(depart, arrivee):
+	arrivee = geocoder(arrivee)
+	base = 'https://maps.googleapis.com/maps/api/directions/json?'
+	key = 'AIzaSyCBQSQ2Ze-8wEnZcT1V8__ug2WLdRmtdmA'
+	url = '{0}'
+
+@conducteurbp.route('/', methods=['GET', 'POST'])
 def conducteur_accueil():
 	p = 'Thibault'
 	return render_template('conducteur/accueil.html', titre='Conducteur', prenom=p)
