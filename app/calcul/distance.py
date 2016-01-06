f8d2d25558dd646da9e003fe887174ca58b91815
CassieChausse <chausse.cassie@gmail.com>
2016-01-06 17:19:01 +0100

    06/11 17:18 G4

--- app/calcul/distance.py
+++ app/calcul/distance.py
@@ -14,32 +14,30 @@ du traffic
 from app.outils import utile
 import json
 
-class Parcours:
-    ''' Classe définissant un parcours :
-    - départ : lieu de départ
-    - arrive : lieu d'arrivé
-    - heure_debut : heure de départ
-    - distance : distance en km
-    - temps : durée du trajet en minutes
-    '''
-    ''' !! Penser à rendre la clef secrete pour l'application finale '''
+def parcours(depart, arrivee, heure_debut):
+    '''Fonction permettant de formater l'url et de calculer les distances
+    Attention à la key qu'il faudra changer'''
+    base = 'https://maps.googleapis.com/maps/api/directions/json?'
+    origine = 'origin=' + utile.formatage_url(depart)
+    destination = '&destination=' + utile.formatage_url(arrivee)
+    departure_time = '&departure_time=' + str(round(utile.convert_date(heure_debut)))
+    fin_url = '&traffic_model=pessimistic&mode=driving&language=fr-FR&key=AIzaSyCQnaoaMu6GVo3AwRzN62l0onao2TPN_u0'
     
-    def __init__(self,depart, arrivee, heure_debut, date_format = "%Y-%m-%d %H:%M:%S"):
-        self.depart = depart
-        self.arrivee = arrivee
-        self.heure_debut = heure_debut
-        
-        base = 'https://maps.googleapis.com/maps/api/directions/json?'
-        origine = 'origin=' + utile.formatage_url(depart)
-        destination = '&destination=' + utile.formatage_url(arrivee)
-        departure_time = '&departure_time=' + str(round(utile.convert_date(heure_debut,date_format)))
-        fin_url = '&traffic_model=pessimistic&mode=driving&language=fr-FR&key=AIzaSyCQnaoaMu6GVo3AwRzN62l0onao2TPN_u0'
-    
-        '''Ajouter l'heure de depart à l'url'''
-        url = base + origine + destination + departure_time + fin_url
-        reponse = utile.requete_http(url)
-        data_response = json.loads(reponse)
-        
-        self.distance_trajet=  round(data_response['routes'][0]['legs'][0]['distance']['value'] / 1000,2)
-        self.temps_trajet= round(data_response['routes'][0]['legs'][0]['duration_in_traffic']['value'] / 60,2)
+    '''Ajouter l'heure de depart à l'url'''
+    url = base + origine + destination + departure_time + fin_url
+    reponse = utile.requete_http(url)
+    return json.loads(reponse)
 
+
+def get_distance(json):
+    '''Fonction permettant de récupérer la distance entre deux points'''
+    resultat = json['routes'][0]['legs'][0]['distance']['value']
+    resultat = round(resultat / 1000,2)
+    return resultat
+
+       
+def get_heure(json):
+    '''Fonction permettant de récuperer le temps de parcours entre deux points'''
+    resultat = json['routes'][0]['legs'][0]['duration_in_traffic']['value']
+    resultat = round(resultat / 60)
+    return resultat
