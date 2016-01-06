23245f2ed337fff182cf0200d068bc8002f038bd
ThibaultLejaille <Thibault.lejaille@gmail.com>
2016-01-06 16:35:10 +0100

    Modif Thibault

--- app/vues/conducteur.py
+++ app/vues/conducteur.py
@@ -1,16 +1,60 @@
 from flask import Blueprint, render_template
 from app.outils.geographie import geocoder
-
+from app import app, db, modeles
+import psycopg2
+import psycopg2.extras
+import sys
 # Créer un patron pour les vues conducteurs
 conducteurbp = Blueprint('conducteurbp', __name__, url_prefix='/conducteur')
 
 
 def gps(depart, arrivee):
-	arrivee = geocoder(arrivee)
-	base = 'https://maps.googleapis.com/maps/api/directions/json?'
-	key = 'AIzaSyCBQSQ2Ze-8wEnZcT1V8__ug2WLdRmtdmA'
-	url = '{0}'
+    arrivee = geocoder(arrivee)
+    base = 'https://maps.googleapis.com/maps/api/directions/json?'
+    key = 'AIzaSyCBQSQ2Ze-8wEnZcT1V8__ug2WLdRmtdmA'
+    url = '{0}'
 
 @conducteurbp.route('/<numero>', methods=['GET', 'POST'])
 def conducteur_accueil(numero):
-	return render_template('conducteur/accueil.html', titre='Conducteur', numero=numero)
+    
+    inp = db.session.execute("SELECT prenom, nom, ST_X(position) as lon, ST_Y(position) as lat, libre FROM conducteurs WHERE telephone = {0}".format(numero))
+    inf = inp.fetchall()
+    pnom = inf[0][0]
+    nom = inf[0][1]
+    lon = inf[0][2]
+    lat = inf[0][3]
+    libre = inf[0][4]
+    return render_template('conducteur/accueil.html', titre='Conducteur', numero=numero, nom = nom, lon=lon, lat=lat, libre=libre)
+
+
+
+
+@app.route('/conducteur/accueil', methods=['GET', 'POST'])
+def statutActuel():
+	lib = db.session.execute("SELECT libre FROM conducteurs where email='amiraayadi@wanadoo.fr'")
+	rep = lib.fetchall()
+	rep=str(rep)
+	rep = rep[2]
+	if rep=="T": 
+		reponse= "Vous êtes actuellement libre"
+	else: reponse="Vous êtes actuellement occupé"
+	return render_template("conducteur/accueil.html",reponse = reponse)
+	
+@app.route('/occupe', methods=['GET', 'POST'])
+def occupe():
+	db.session.execute("UPDATE conducteurs SET libre = false where email='amiraayadi@wanadoo.fr'" )
+	db.session.commit()
+	lib1 = db.session.execute("SELECT libre FROM conducteurs where email='amiraayadi@wanadoo.fr'")
+	rc = lib1.fetchall()
+	return rc
+	
+@app.route('/libre', methods=['GET', 'POST'])
+def libre():
+	db.session.execute("UPDATE conducteurs SET libre = true where email='amiraayadi@wanadoo.fr'" )
+	db.session.commit()
+ 
+ 
+@app.route('/majlat', methods=['GET', 'POST'])
+def majlat(lon, lat):
+    db.session.execute("UPDATE conducteurs SET ST_X(position) ={0} and ST_X(position) ={1} true where email='amiraayadi@wanadoo.fr'".format(lat,lon) )
+    db.session.commit()
