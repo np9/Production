0a541478bbd72bbdc9525d1b73fca186b72f18f4
FlorenceCanal <florencecanal1@gmail.com>
2016-01-07 17:13:13 +0100

    07/01 17:13 G7

--- app/vues/api.py
+++ app/vues/api.py
@@ -3,6 +3,7 @@ from app import app, db
 from app.formulaires import utilisateur as fu
 import pandas as pd
 import datetime
+import time
 
 apibp = Blueprint('apibp', __name__, url_prefix='/api')
 
@@ -18,8 +19,8 @@ def nettoyer(colonne):
     return colonne
 
 
-def to_json(table):
-    requete = db.session.execute("SELECT * FROM {}".format(table))
+def to_json(numero):
+    requete = db.session.execute("SELECT * FROM conducteurs WHERE telephone = {}".format(numero))
     attributs = requete.keys()
     lignes = requete.fetchall()
     table = pd.DataFrame(lignes, columns=attributs)
@@ -28,10 +29,46 @@ def to_json(table):
     return json
 
 
-@apibp.route('/<table>', methods=['GET'])
-def api_table(table):
+@apibp.route('/conducteurs/<numero>', methods=['GET'])
+def api_table(numero):
     try:
-        json = to_json(table)
+        json = to_json(numero)
+        return jsonify({'data': json, 'status': 'success'})
+    except:
+        return jsonify({'status': 'failure'})
+   
+@apibp.route('/conducteurs/maj_position/numero=<numero>&lat=<lat>&long=<long>', methods=['GET'])
+def maj_position(numero, lat, long):
+    try:
+        db.session.execute("UPDATE conducteurs SET position = 'POINT({0} {1})' where telephone = '{2}' ".format(lat, long, numero))
+        db.session.commit()        
+        return jsonify({'status': 'success'})
+    except:
+        return jsonify({'status': 'failure'})
+        
+@apibp.route('/conducteurs/maj_statut/numero=<numero>&statut=<statut>', methods=['GET'])
+def maj_statut(numero, statut):
+    try:
+        db.session.execute("UPDATE conducteurs SET statut = '{0}' where telephone = '{1}' ".format(statut, numero) )
+        db.session.commit()
+        return jsonify({'status': 'success'})
+    except:
+        return jsonify({'status': 'failure'})
+        
+def demande_course(numero):
+    requete = db.session.execute("SELECT * FROM propositions p, courses c WHERE p.course = c.numero AND p.ordre = 1 AND p.conducteur = {}".format(numero))
+    attributs = requete.keys()
+    lignes = requete.fetchall()
+    table = pd.DataFrame(lignes, columns=attributs)
+    table = table.apply(nettoyer)
+    json = table.to_dict(orient='records')
+    return json
+
+
+@apibp.route('/conducteurs/demande_course/<numero>', methods=['GET'])
+def api_demande_course(numero):
+    try:
+        json = demande_course(numero)
         return jsonify({'data': json, 'status': 'success'})
     except:
         return jsonify({'status': 'failure'})
