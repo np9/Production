1e4aba75a55e598d96e3a1bf31f0aa2702d41130
CassieChausse <chausse.cassie@gmail.com>
2016-01-07 11:48:06 +0100

    07/01 11:47 G7

--- app/vues/conducteur.py
+++ app/vues/conducteur.py
@@ -17,7 +17,7 @@ def gps(depart, arrivee):
 @conducteurbp.route('/<numero>', methods=['GET', 'POST'])
 def conducteur_accueil(numero):
     
-    inp = db.session.execute("SELECT prenom, nom, ST_X(position) as lon, ST_Y(position) as lat, libre FROM conducteurs WHERE telephone = {0}".format(numero))
+    inp = db.session.execute("SELECT prenom, nom, ST_X(position) as lon, ST_Y(position) as lat, statut FROM conducteurs WHERE telephone = {0}".format(numero))
     inf = inp.fetchall()
     pnom = inf[0][0]
     nom = inf[0][1]
@@ -25,9 +25,6 @@ def conducteur_accueil(numero):
     lat = inf[0][3]
     libre = inf[0][4]
     return render_template('conducteur/accueil.html', titre='Conducteur', numero=numero, nom = nom, lon=lon, lat=lat, libre=libre)
-
-
-
 
 @app.route('/conducteur/accueil', methods=['GET', 'POST'])
 def statutActuel():
@@ -39,7 +36,7 @@ def statutActuel():
 		reponse= "Vous êtes actuellement libre"
 	else: reponse="Vous êtes actuellement occupé"
 	return render_template("conducteur/accueil.html",reponse = reponse)
-	
+
 @app.route('/occupe', methods=['GET', 'POST'])
 def occupe():
 	db.session.execute("UPDATE conducteurs SET libre = false where email='amiraayadi@wanadoo.fr'" )
@@ -58,3 +55,18 @@ def libre():
 def majlat(lon, lat):
     db.session.execute("UPDATE conducteurs SET ST_X(position) ={0} and ST_X(position) ={1} true where email='amiraayadi@wanadoo.fr'".format(lat,lon) )
     db.session.commit()
+
+
+'''
+@app.route('/Attribution_Thomas') #Propositions de courses possibles
+def Attribution_Thomas():
+	lib = db.session.execute("SELECT numero, course, debut, fin FROM propositions P, courses C WHERE P.course = C.numero")
+	rep = lib.fetchall()
+	num = rep[0][1] #recuperation du numero de course de la première possible
+	#recuperation seulement de l'heure de début et de fin
+	a = rep[0][2]
+	deb = a.isoformat()[11:19]
+	b = rep[0][3]
+	fin = b.isoformat()[11:19]
+	return render_template("Attribution_Thomas.html", num = num, deb = deb, fin = fin)"
+'''
