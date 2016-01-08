3061192917c69a8bc218af8533360397494e4996
CassieChausse <chausse.cassie@gmail.com>
2016-01-08 15:17:19 +0100

    ajout des tests dans le .sh

--- testBD.sh
+++ testBD.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+
+# on installe les packages nécessaire
 pip install -r requirements.txt
+# on supprime l'ancienne BD
 python suppression.py
+# on fait les tests
+py.test
+# on crée la BD
 python creation.py
+# on rempli la BD
 python insertions.py
+# on lance l'application
 python run.py
